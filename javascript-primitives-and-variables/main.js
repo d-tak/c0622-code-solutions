92521fadc75bd7c0f06c1506134208907f98e1c2
Daniel Tak <danieljtak@hotmail.com>
2022-07-05 18:58:08 +0000

    Completed javascript-primitives-and-variables exercise. - Daniel Tak

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,15 @@
+var fullName = 'Daniel Tak';
+var isCool = false;
+var totalPets = 0;
+
+console.log('value of fullName:', fullName);
+console.log('value of isCool', isCool);
+console.log('value of totalPets', totalPets);
+
+var unknown = null;
+console.log('value of unknown', unknown);
+console.log('type of unknown', typeof unknown);
+
+var never;
+console.log('value of never', never);
+console.log('type of never', typeof never);
