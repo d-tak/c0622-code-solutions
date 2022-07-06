8b7dc601937a851271b62fad70c2abfef70d49a3
Daniel Tak <danieljtak@hotmail.com>
2022-07-06 23:42:21 +0000

    updated var names. - Daniel Tak

--- javascript-functions/main.js
+++ javascript-functions/main.js
@@ -7,40 +7,40 @@ var addTwoNumbersResult = addTwoNumbers(2, 3);
 console.log('addTwoNumbersResult:', addTwoNumbersResult);
 
 function convertMinutesToSeconds(minutes) {
-  var sum = minutes * 60;
-  return sum;
+  var seconds = minutes * 60;
+  return seconds;
 }
 
 var convertMinutesToSecondsResult = convertMinutesToSeconds(5);
 console.log('convertMinutesToSecondsResult:', convertMinutesToSecondsResult);
 
 function greet(name) {
-  var sum = '"Hey, ' + name + '"';
-  return sum;
+  var greeting = '"Hey, ' + name + '"';
+  return greeting;
 }
 
 var greetResult = greet('Beavis');
 console.log('greetResult:', greetResult);
 
 function getArea(width, height) {
-  var sum = width * height;
-  return sum;
+  var area = width * height;
+  return area;
 }
 
 var getAreaResult = getArea(17, 42);
 console.log('getAreaResult:', getAreaResult);
 
 function getFirstName(person) {
-  var sum = person.firstName;
-  return sum;
+  var firstName = person.firstName;
+  return firstName;
 }
 
 var getFirstNameResult = getFirstName({ firstName: 'Lelouche', lastName: 'Lamperouge' });
 console.log('getFirstNameResult:', getFirstNameResult);
 
 function getLastElement(array) {
-  var sum = array[array.length - 1];
-  return sum;
+  var lastElement = array[array.length - 1];
+  return lastElement;
 }
 
 var getLastElementResult = getLastElement(['propane', 'and', 'propone', 'accessories']);
