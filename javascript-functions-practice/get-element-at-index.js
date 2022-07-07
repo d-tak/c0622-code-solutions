c1a6bd995e79c3f16321c4b95e5179e581a53275
Daniel Tak <danieljtak@hotmail.com>
2022-07-07 20:42:17 +0000

    completed javascript functions practice

---
+++ javascript-functions-practice/get-element-at-index.js
@@ -0,0 +1,5 @@
+/* exported getElementAtIndex */
+function getElementAtIndex(array, index) {
+  var elementAtIndex = array.at(index);
+  return elementAtIndex;
+}
---
+++ javascript-functions-practice/get-second-character.js
@@ -0,0 +1,5 @@
+/* exported getSecondCharacter */
+function getSecondCharacter(string) {
+  var secondCharacter = string.substr(1, 1);
+  return secondCharacter;
+}
