4c4c9adc0fd0e014064d5f2290cfc558b2eb3978
Daniel Tak <danieljtak@hotmail.com>
2022-07-08 15:27:29 +0000

    Updated categories for order history. - Daniel Tak

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -6,11 +6,17 @@ var orderHistory = [
     shipTo: 'JS Masher',
     deliveryDate: 'Delivered August 8, 2020',
     dropStatus: null,
-    itemDescription: ['JavaScript for impatient programmers'],
-    authorLastName: ['Rauschmayer'],
-    authorFirstName: ['Dr. Axel'],
-    returnWindowStatus: ['Return window closed on Sep 7, 2020'],
-    itemCost: ['$31.55']
+
+    productDetails: [
+      {
+        productCategory: 'textbook',
+        itemDescription: 'JavaScript for impatient programmers',
+        authorLastName: 'Rauschmayer',
+        authorFirstName: 'Dr. Axel',
+        returnWindowStatus: 'Return window closed on Sep 7, 2020',
+        itemCost: '$31.55'
+      }
+    ]
   },
   {
     orderPlaced: 'July 19, 2020',
@@ -19,11 +25,17 @@ var orderHistory = [
     shipTo: 'JS Masher',
     deliveryDate: 'Delivered Jul 20, 2020',
     dropStatus: 'Your package was delivered. It was handed directly to a resident.',
-    itemDescription: ['The Timeless Way of Building'],
-    authorLastName: ['Alexander'],
-    authorFirstName: ['Christopher'],
-    returnWindowStatus: ['Return window closed on Aug 19, 2020'],
-    itemCost: ['$41.33']
+
+    productDetails: [
+      {
+        productCategory: 'textboook',
+        itemDescription: 'The Timeless Way of Building',
+        authorLastName: 'Alexander',
+        authorFirstName: 'Christopher',
+        returnWindowStatus: 'Return window closed on Aug 19, 2020',
+        itemCost: '$41.33'
+      }
+    ]
   },
   {
     orderPlaced: 'July 4, 2020',
@@ -32,11 +44,17 @@ var orderHistory = [
     shipTo: 'JS Masher',
     deliveryDate: 'Delivered Jul 7, 2020',
     dropStatus: 'Your package was delivered. It was handed directly to a resident.',
-    itemDescription: ['Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter'],
-    authorLastName: [null],
-    authorFirstName: [null],
-    returnWindowStatus: ['Return window closed on Aug 5, 2020'],
-    itemCost: ['$15.98']
+
+    productDetails: [
+      {
+        productCategory: 'gaming',
+        itemDescription: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+        authorLastName: null,
+        authorFirstName: null,
+        returnWindowStatus: 'Return window closed on Aug 5, 2020',
+        itemCost: '$15.98'
+      }
+    ]
   },
   {
     orderPlaced: 'July 3, 2020',
@@ -45,10 +63,24 @@ var orderHistory = [
     shipTo: 'JS Masher',
     deliveryDate: 'Delivered Jul 5, 2020',
     dropStatus: null,
-    itemDescription: ['GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)', 'The Art of Sql'],
-    authorLastName: ['', 'Faroult'],
-    authorFirstName: ['', 'Stephanie'],
-    returnWindowStatus: ['Return window closed on Aug 4, 2020', 'Return window closed on Aug 4, 2020'],
-    itemCost: ['$94.95', '$33.99']
+
+    productDetails: [
+      {
+        productCategory: 'gaming',
+        itemDescription: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        authorLastName: null,
+        authorFirstName: null,
+        returnWindowStatus: 'Return window closed on Aug 4, 2020',
+        itemCost: '$94.95'
+      },
+      {
+        productCategory: 'textbook',
+        itemDescription: 'The Art of Sql',
+        authorLastName: 'Faroult',
+        authorFirstName: 'Stephanie',
+        returnWindowStatus: 'Return window closed on Aug 4, 2020',
+        itemCost: '$33.99'
+      }
+    ]
   }
 ];
