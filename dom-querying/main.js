29dab7b9e5858c65bf3637ee4f42d2facf2a9c68
Daniel Tak <danieljtak@hotmail.com>
2022-07-11 19:29:48 +0000

    Completed dom-querying exercise. - Daniel Tak

---
+++ dom-querying/main.js
@@ -0,0 +1,23 @@
+console.log('hello, world');
+
+var newVar = document.querySelector('h1');
+
+console.log('newVar', newVar);
+console.dir(newVar);
+
+document.querySelector('#explanation');
+
+console.log("document.querySelector('#explanation')", document.querySelector('#explanation'));
+console.dir(document.querySelector('#explanation'));
+
+var hint = document.querySelector('.hint');
+console.log('hint', hint);
+console.dir(hint);
+
+var p = document.querySelectorAll('p');
+console.log('p', p);
+console.dir(p);
+
+var links = document.querySelectorAll('.example-link');
+console.log('links', links);
+console.dir(links);
