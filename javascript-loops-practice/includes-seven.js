1595f72d527b9f7f07ddf3c8baf1832b4a23fb47
Daniel Tak <danieljtak@hotmail.com>
2022-07-12 23:24:55 +0000

    COMPLETED javascript-loops-practice exercise. - Daniel Tak

--- javascript-loops-practice/includes-seven.js
+++ javascript-loops-practice/includes-seven.js
@@ -1,9 +1,27 @@
 /* exported includesSeven */
 
-// function includesSeven(array) {
-//   for (var i = 0; i < array.length; i++) {
-//     if (array[i] === 7) {
-//       return true;
-//     } else {
-//     return false;
-//   }
+/* GOAL: go through all numbers and find any value of 7
+by running a loop and return a boolean result (true/false)
+
+create a function to find all forms of the value 7
+function includesSeven (array)
+
+create a way to go through and check all of the numbers entered
+(create a loop (eg. for (var i = 0; i < values.length; i++))
+
+find if the value 7 is present by going through each value of the array.
+(if (array[i] === 7))
+
+If 7 is found, return a boolean value of true.
+Otherwise, return false.
+
+*/
+
+function includesSeven(array) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === 7) {
+      return true;
+    }
+  }
+  return false;
+}
