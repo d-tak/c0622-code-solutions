56f24250721523eefa6c15cde082c9d83f13d00a
Daniel Tak <danieljtak@hotmail.com>
2022-07-14 15:17:25 +0000

    Completed array-manipulation-1 exercise. - Daniel Tak

--- array-manipulation-1/src/reverse.js
+++ array-manipulation-1/src/reverse.js
@@ -1,1 +1,18 @@
 /* exported reverse */
+
+/* GOAL A new array containing all elements of array in reverse order.
+1. Create Function (given)
+2. Create a new empty array to push in values
+3. Create a loop to go through the Array (BACKWARDS)
+4. initialize at last index.
+5.
+*/
+
+function reverse(array) {
+  var newArray = [];
+
+  for (var i = array.length - 1; i >= 0; i--) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+}
