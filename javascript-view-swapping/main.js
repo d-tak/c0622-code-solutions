fea18ef2daf9208537401eda265501fde4e89f11
Daniel Tak <danieljtak@hotmail.com>
2022-07-15 02:32:19 +0000

    Completed javascript-view-swapping exercise. - Daniel Tak

---
+++ javascript-view-swapping/main.js
@@ -0,0 +1,20 @@
+
+// var newVarForNow = event.target('data-view')
+
+// for(var i = 0; i < 0 ; i++) {
+//   if ('data-view') {
+//     return .active
+//     }  else if (!==.active) {
+//       return '.hidden'
+//     }
+//     return newVarForNow
+
+//   }
+// }
+
+// if(event.target.matches('.tab') {
+//   for(NodeList of '.tab') {
+//     === event.target
+//     }
+//   }
+// }
