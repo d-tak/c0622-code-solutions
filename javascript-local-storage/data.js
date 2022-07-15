a7617f602ade92281ffa79b67f4299c4ae0cb94e
Daniel Tak <danieljtak@hotmail.com>
2022-07-15 23:49:58 +0000

    Completed javascript-local-storage exercise. - Daniel Tak

--- javascript-local-storage/data.js
+++ javascript-local-storage/data.js
@@ -1,3 +1,15 @@
 /* exported todos */
 
 var todos = [];
+var previousTodosJSON = localStorage.getItem('javascript-local-storage');
+
+if (previousTodosJSON !== null) {
+  todos = JSON.parse(previousTodosJSON);
+}
+
+window.addEventListener('beforeunload', saveList);
+
+function saveList(event) {
+  var todosJSON = JSON.stringify(todos);
+  localStorage.setItem('javascript-local-storage', todosJSON);
+}
