da85647985072ecec989012b3ad938854cee8d74
Daniel Tak <danieljtak@hotmail.com>
2022-07-18 01:50:07 +0000

    Completed javascript-forms exercise. - Daniel Tak

--- javascript-forms/form-events.js
+++ javascript-forms/form-events.js
@@ -1,32 +1,26 @@
 function handleFocus(event) {
-  console.log('focus was fired');
-  console.log(event.target.name);
-
+  console.log('focus event was fired');
+  console.log('event.target.name', event.target.name);
 }
-document.querySelector('#user-name');
-var $idUser = document.querySelector('#user-name');
-$idUser.addEventListener('focus', handleFocus);
-$idUser.addEventListener('focus', handleBlur);
-$idUser.addEventListener('focus', handleInput);
+var userName = document.querySelector('#user-name');
+userName.addEventListener('focus', handleFocus);
+userName.addEventListener('blur', handleBlur);
+userName.addEventListener('input', handleInput);
 
 function handleBlur(event) {
-  console.log('blur was fired');
-  console.log(event.target.name);
-
+  console.log('blur event was fired');
+  console.log('event.target.name', event.target.name);
 }
-var $idEmail = document.querySelector('#user-email');
-$idEmail.addEventListener('blur', handleFocus);
-$idEmail.addEventListener('blur', handleBlur);
-$idEmail.addEventListener('blur', handleInput);
+var userEmail = document.querySelector('#user-email');
+userEmail.addEventListener('focus', handleFocus);
+userEmail.addEventListener('blur', handleBlur);
+userEmail.addEventListener('input', handleInput);
 
 function handleInput(event) {
-  console.log('input was fired');
-  console.log(event.target.name);
-
+  console.log('event.target.name', event.target.name);
+  console.log('event.target.value', event.target.value);
 }
-var $idUserMessage = document.querySelector('#user-message');
-$idUserMessage.addEventListener('input', handleFocus);
-$idUserMessage.addEventListener('input', handleBlur);
-$idUserMessage.addEventListener('input', handleInput);
-
-//
+var userMessage = document.querySelector('#user-message');
+userMessage.addEventListener('focus', handleFocus);
+userMessage.addEventListener('blur', handleBlur);
+userMessage.addEventListener('input', handleInput);
