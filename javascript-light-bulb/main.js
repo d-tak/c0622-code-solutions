758b8b4628d9c7bee555745332d58e6b2f3cb690
Daniel Tak <danieljtak@hotmail.com>
2022-07-19 04:18:13 +0000

    Completed javascript-light-bulb challenge exercise. - Daniel Tak

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,8 +1,18 @@
-// var toggle =
-// });
-// click = function () {
-//   toggle.classList.circle('on');
-// };
+var lightbulb = true;
+var circleOn = document.querySelector('.circle-on');
+var circleOff = document.querySelector('.circle-off');
+var containerOn = document.querySelector('.container.on');
 
-// function click() {
-//   var circle = document.querySelector('circle');
+function toggle(event) {
+  if (lightbulb === true) {
+    circleOn.className = 'circle-on';
+    containerOn.className = 'container on';
+    lightbulb = false;
+  } else if (lightbulb === false) {
+    circleOn.className = 'circle-off';
+    containerOn.className = 'container off';
+    lightbulb = true;
+  }
+}
+circleOn.addEventListener('click', toggle);
+circleOff.addEventListener('click', toggle);
