c1a26876f28dcc17897894b6f638f858f24005e4
Daniel Tak <danieljtak@hotmail.com>
2022-08-17 19:40:15 +0000

    added comments from q&a

--- node-require/calculate.js
+++ node-require/calculate.js
@@ -5,7 +5,8 @@ const divide = require('./divide');
 
 const x = parseInt(process.argv[2]);
 const opr = process.argv[3];
-const y = Number(process.argv[4]);
+const y = +process.argv[4];
+// can use '+' instead of Number()
 
 if (opr === 'plus') {
   console.log('result:', add(x, y));
@@ -16,3 +17,10 @@ if (opr === 'plus') {
 } else if (opr === 'over') {
   console.log('result:', divide(x, y));
 }
+
+// console.log(process.argv[0]);
+// console.log(process.argv[1]);
+// console.log(process.argv[2]);
+// console.log(process.argv[3]);
+// console.log(process.argv[4]);
+// console.log(process.argv[5]);
