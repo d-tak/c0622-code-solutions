98ba7570d7ea3ff8b9942571a5f724a48768fc31
Daniel Tak <danieljtak@hotmail.com>
2022-08-18 00:02:52 +0000

    added comments from Q&A

--- node-fs-readfile/read-any.js
+++ node-fs-readfile/read-any.js
@@ -4,3 +4,10 @@ fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) throw err;
   console.log(data);
 });
+
+// if(err) { (if doesn't necessarily need true or false but needs truthy or falsy).
+//   console.error(err);
+//   process.exit(1);
+// }
+// console.log(text);
+// });
