c679bdb61ad1c80c2349b128d395915ca0b177e7
Daniel Tak <danieljtak@hotmail.com>
2022-08-29 19:57:04 +0000

    Included method definitions within the class body.

--- es6-classes/student.js
+++ es6-classes/student.js
@@ -6,9 +6,17 @@ class Student {
     this.subject = subject;
   }
 
-  getStudent() {
-    return this.Student;
+  getFullName() {
+    const { firstName, lastName } = this;
+    return `${firstName} ${lastName}`;
   }
+
+  getIntroduction() {
+    const { subject } = this;
+    const fullName = this.getFullName();
+    return `Hello, my name is ${fullName} and I am studying ${subject}.`;
+  }
+
 }
 
 // function Student(firstName, lastName, subject) {
@@ -16,14 +24,3 @@ class Student {
 //   this.lastName = lastName;
 //   this.subject = subject;
 // }
-
-Student.prototype.getFullName = function () {
-  const { firstName, lastName } = this;
-  return `${firstName} ${lastName}`;
-};
-
-Student.prototype.getIntroduction = function () {
-  const { subject } = this;
-  const fullName = this.getFullName();
-  return `Hello, my name is ${fullName} and I am studying ${subject}.`;
-};
