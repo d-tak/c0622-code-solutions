43bbdf65c4562ebed84ef59b3d4b9f48f07ead50
Daniel Tak <danieljtak@hotmail.com>
2022-11-11 19:53:58 +0000

    updated list to pass as prop in index.jsx

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -1,23 +1,4 @@
 import React from 'react';
-
-const list = [
-  {
-    id: 1,
-    subject: 'Hypertext Markup Language',
-    description: 'Hypertext Markup Language (HTML) is the standard markup language for creating web pages and web applications.  With Cascading Style Sheets (CSS) and JavaScript, it forms a triad of cornerstone technologies for the World Wide Web.'
-  },
-  {
-    id: 2,
-    subject: 'Cascading Style Sheet',
-    description: 'Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup langugage like HTML.  CSS is a cornerstone technology of the World Wide Web alongside HTML and JavaScript.'
-  },
-  {
-    id: 3,
-    subject: 'JavaScript',
-    description: 'JavaScript, often abbreviated as JS, is a high-level, interpreted programming language that conforms to the ECMAScript specification.  JavaScript has curly-bracket syntax, dynamic typing, prototype-based object orientation, and first-class functions.'
-  }
-];
-
 export default class Accordion extends React.Component {
   constructor(props) {
     super(props);
@@ -40,7 +21,7 @@ export default class Accordion extends React.Component {
     let tab;
     return (
       <ul>
-        {list.map((language, id) => {
+        {this.props.list.map((language, id) => {
           if (this.state.id === language.subject) {
             tab = 'yomama';
           } else {
